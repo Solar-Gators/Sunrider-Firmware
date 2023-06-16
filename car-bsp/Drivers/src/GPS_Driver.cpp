dc10c06a25f164201ecec064ee0353493bd119ae
Yash Bhat <68254066+yashtheboss@users.noreply.github.com>
2023-06-16 18:14:56 -0400

    excluded gps drver from non telem builds

--- car-bsp/Drivers/src/GPS_Driver.cpp
+++ car-bsp/Drivers/src/GPS_Driver.cpp
@@ -8,7 +8,7 @@
 //Private Constants
 #define RX_MESSAGE_MAX_SIZE 100
 //Private Variables
-static UART_HandleTypeDef* uartInstance;
+//static UART_HandleTypeDef* uartInstance;
 static char rxMessage[RX_MESSAGE_MAX_SIZE];
 static uint8_t messageStart = 0;
 static uint16_t rxMessageIndex = 0;
@@ -56,7 +56,7 @@ char* GPS_RxCpltCallback(bool* success, char rxChar)
 	{
 		rxMessage[rxMessageIndex++] = rxChar;
 	}
-	if(rxChar == '\n')
+	if(rxChar == 0x0A)
 	{
 		rxMessage[rxMessageIndex] = '\0';
 		messageStart = 0;
